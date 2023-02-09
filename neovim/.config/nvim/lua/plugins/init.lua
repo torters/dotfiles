cc0dd5630a213e9573e0a22715a833ab98ae5b8d
db0x0db <db0x0db@proton.me>
2023-02-09 12:41:02 +0800

    neovim: add Raimondi/delimitMate

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
-    'RRethy/nvim-base16',
-    'b3nj5m1n/kommentary',
+    "RRethy/nvim-base16",
+    "b3nj5m1n/kommentary",
+    {
+        "Raimondi/delimitMate",
+        config = function()
+            vim.g.delimitMate_expand_cr = 1
+        end,
+    },
 }
